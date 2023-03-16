e68507fd5e18a86fc1a2d0d918ce78dce930e146
radhikaallapuraju <radhikaallapuraju@gmail.com>
2023-03-16 18:51:49 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def NTR(request):
+    return render(request,'NTR.html')
+
---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def VIJAY(request):
+    return render(request,'VIJAY.html')
